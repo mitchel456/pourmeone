935803089440db873ef2c2914ca3d7f66072701a
Ryan Mitchell <mitchellryanj@gmail.com>
2013-01-15 21:27:17 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pourmeone::Application.config.secret_token = '517d90be37aca14b6eb8880d1cf3b581c298991f398a190d770c4eb7d85edf0c1ccdd6cc3685b3dd3e256f217c29bb4bb990840faf963ca794dd4b3af0eaeef3'
