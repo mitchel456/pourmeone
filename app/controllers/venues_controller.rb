946f771e071205b0dd7d326310820c2a422ff1a0
Ryan Mitchell <mitchellryanj@gmail.com>
2013-04-21 16:34:31 -0700

    Add category id

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -7,9 +7,13 @@ class VenuesController < ApplicationController
       :client_id => ENV['FOURSQUARE_CLIENT_ID'],
       :client_secret => ENV['FOURSQUARE_CLIENT_SECRET']
     )
-    results = client.search_venues(ll: params[:lat] + ',' + params[:lon], query: params[:term], limit: 10)
+    results = client.search_venues(l
+      l: params[:lat] + ',' + params[:lon], 
+      query: params[:term], 
+      limit: 10,
+      categoryid: '4d4b7105d754a06376d81259')
     json = results.groups.first.items.map do |place|
-      { value: place.id, label: place.name }
+      { value: place.id, label: place.name, source: 'foursquare' }
     end
     render json: json
   end
