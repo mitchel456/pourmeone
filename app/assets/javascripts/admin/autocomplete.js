0cd17b82471dcd311534fce157c6a916e30315aa
Ryan Mitchell <mitchellryanj@gmail.com>
2013-08-04 16:17:52 -0700

    Attach autocompletes when a new ingredient is added in the admin page

--- app/assets/javascripts/admin/autocomplete.js
+++ app/assets/javascripts/admin/autocomplete.js
@@ -1,31 +1,39 @@
 $(function() {
-  $.each($('select'), function(index, select) {
-    var combobox = $('<input/>'),
-        hidden = $('<input/>', { type: 'hidden', name: $(select).attr('name') }),
-        currentValue = $(select).find(':selected').text();
-        options = $.map(select, function(option) {
-          return {
-            label: option.label,
-            value: option.value
-          }
-        });
-    combobox.autocomplete({
-      source: $.map(options, function(option) { return option.label }),
-      select: function(event, ui) {
-        var id;
-        $.each(options, function(index, option) {
-          if (option.label == ui.item.label) {
-            id = option.value;
-          }
-        });
-        hidden.val(id);
+
+  var attachAutocompletes = function() {
+    $.each($('select'), function(index, select) {
+      var combobox = $('<input/>'),
+          hidden = $('<input/>', { type: 'hidden', name: $(select).attr('name') }),
+          currentValue = $(select).find(':selected').text();
+      options = $.map(select, function(option) {
+        return {
+          label: option.label,
+          value: option.value
+        }
+      });
+      combobox.autocomplete({
+        source: $.map(options, function(option) { return option.label }),
+        select: function(event, ui) {
+          var id;
+          $.each(options, function(index, option) {
+            if (option.label == ui.item.label) {
+              id = option.value;
+            }
+          });
+          hidden.val(id);
+        }
+      });
+      if (currentValue) {
+        combobox.val(currentValue);
       }
+      combobox.insertAfter(select);
+      hidden.insertAfter(select);
+      $(select).remove();
     });
-    if (currentValue) {
-      combobox.val(currentValue);
-    }
-    combobox.insertAfter(select);
-    hidden.insertAfter(select);
-    $(select).remove();
+  };
+
+  $('.drink_ingredients > a.button').on('click', function() {
+    attachAutocompletes();
   });
+  attachAutocompletes();
 });
