10f270a098824b319a91f805c1749f2e93e5c41d
Ryan Mitchell <mitchellryanj@gmail.com>
2013-09-25 23:15:20 -0700

    Correctly set up ingredients on post as well as put

--- app/admin/drinks.rb
+++ app/admin/drinks.rb
@@ -16,6 +16,20 @@ ActiveAdmin.register Drink do
 
   controller do
 
+    def create
+      @drink = Drink.create(permitted_params[:drink])
+      permitted_params[:ingredients].each do |drink_ingredient|
+        if drink_ingredient[:id].empty?
+          ingredient = Ingredient.create(name: drink_ingredient[:name])
+        else
+          ingredient = Ingredient.find(drink_ingredient[:id])
+        end
+        @drink.ingredients << ingredient
+      end
+      @drink.save
+      redirect_to [:admin, @drink]
+    end
+
     def update
       @drink = Drink.find(params[:id])
       # remove all ingredients in preparation for re-adding them next
